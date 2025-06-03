0337012b4da8f45b25a615dc3d6aeb7466e9caa6
Beep Iams <beepiams@me.com>
2025-06-02 19:13:09 -0600

    app.js ReactDOM render

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FastClick from 'fastclick';
-import { Router, Route, Link } from 'react-router';
-import routes from './routes';
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import App from './App'; // or your main component
 
-function run(){
-	const reactMountPoint = document.getElementById('app');
-
-	let history = createBrowserHistory();
-
-	ReactDOM.render(<Router onUpdate={() => window.scrollTo(0, 0)} history={history} routes={routes}/>, reactMountPoint );
-}
-
-new Promise((resolve) => {
-    if (window.addEventListener) {
-      window.addEventListener('DOMContentLoaded', resolve);
-    } else {
-      window.attachEvent('onload', resolve);
-    }
-  }).then(run)
+ReactDOM.render(<App />, document.getElementById('app'));

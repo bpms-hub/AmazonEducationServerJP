255dbad5df4387ad46f26b6d142a929cf22a311e
Beep Iams <beepiams@me.com>
2025-06-02 19:17:10 -0600

    app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // or your main component
+import FastClick from 'fastclick';
+import { Router, Route, Link } from 'react-router';
+import routes from './routes';
+import createHashHistory from 'history/lib/createHashHistory'; // <-- change here
 
-ReactDOM.render(<App />, document.getElementById('app'));
+function run(){
+    const reactMountPoint = document.getElementById('app');
+
+    let history = createHashHistory(); // <-- change here
+
+    ReactDOM.render(
+        <Router onUpdate={() => window.scrollTo(0, 0)} history={history} routes={routes}/>,
+        reactMountPoint
+    );
+}
+
+new Promise((resolve) => {
+    if (window.addEventListener) {
+      window.addEventListener('DOMContentLoaded', resolve);
+    } else {
+      window.attachEvent('onload', resolve);
+    }
+  }).then(run)
